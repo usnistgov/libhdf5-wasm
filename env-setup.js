269f334e863ec35ada1f62f9a63cd6914204e3cf
Brian Benjamin Maranville <brian.maranville@nist.gov>
2025-11-07 15:46:16 -0500

    set default plugin path for bun as well (uses process.env)

--- env-setup.js
+++ env-setup.js
@@ -5,17 +5,29 @@ var Module = typeof Module !== 'undefined' ? Module : {};
 Module['preRun'] = Module['preRun'] || [];
 Module['preRun'].push(function() {
   if (typeof process !== 'undefined') {
-    // Use HDF5_PLUGIN_PATH from environment if set
-    if (process.env && process.env.HDF5_PLUGIN_PATH) {
-      ENV['HDF5_PLUGIN_PATH'] = process.env.HDF5_PLUGIN_PATH;
-    }
-    // Otherwise, set a default path relative to the script location
-    else if (typeof __dirname !== 'undefined') {
+    // Determine the default plugin path relative to the script location
+    var defaultPluginPath = null;
+    if (typeof __dirname !== 'undefined') {
       // When installed: __dirname is node_modules/hdf5-wasm-tools/bin
       // We want to look for plugins at: node_modules/h5wasm-plugins/plugins
       var path = require('path');
-      var defaultPluginPath = path.join(__dirname, '..', '..', 'h5wasm-plugins', 'plugins');
-      ENV['HDF5_PLUGIN_PATH'] = defaultPluginPath;
+      defaultPluginPath = path.join(__dirname, '..', '..', 'h5wasm-plugins', 'plugins');
+    }
+
+    // Get the plugin path from environment or use default
+    var pluginPath = (process.env && process.env.HDF5_PLUGIN_PATH) || defaultPluginPath;
+
+    if (pluginPath) {
+      // Set in ENV object (for Node.js/Emscripten)
+      if (typeof ENV !== 'undefined') {
+        ENV['HDF5_PLUGIN_PATH'] = pluginPath;
+      }
+
+      // Also set in process.env for Bun and other runtimes
+      if (process.env) {
+        process.env.HDF5_PLUGIN_PATH = pluginPath;
+      }
     }
   }
 });
+
